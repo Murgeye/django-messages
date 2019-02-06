33fdeb621e71f325ac91d3a141bf17e3956df2c4
HenryMehta <hjsmehta@gmail.com>
2019-02-06 13:38:46 +0000

    recipients hidden

--- django_messages/forms.py
+++ django_messages/forms.py
@@ -15,7 +15,10 @@ class ComposeForm(forms.Form):
     """
     A simple default form for private messages.
     """
-    recipient = CommaSeparatedUserField(label=_(u"Recipient"), attrs={'hidden':True})
+    recipient = CommaSeparatedUserField(
+        label=_(u"Recipient"), 
+        widget=forms.TextInput(attrs={'hidden':True, 'readonly':True})
+    )
     subject = forms.CharField(label=_(u"Subject"), max_length=140)
     body = forms.CharField(label=_(u"Body"),
         widget=forms.Textarea(attrs={'rows': '12', 'cols':'55'}))
