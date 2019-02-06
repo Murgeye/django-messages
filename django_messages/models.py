34c392e7dbc04d647553439b0cfa8c74e6f569fa
HenryMehta <hjsmehta@gmail.com>
2019-02-06 12:41:32 +0000

    add @property to get_absolute_url

--- django_messages/models.py
+++ django_messages/models.py
@@ -77,9 +77,9 @@ class Message(models.Model):
     def __str__(self):
         return self.subject
 
+    @property
     def get_absolute_url(self):
         return ('messages_detail', [self.id])
-    get_absolute_url = models.permalink(get_absolute_url)
 
     def save(self, **kwargs):
         if not self.id:
